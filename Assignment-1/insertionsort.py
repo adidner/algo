e8b7b31f23238d5a3588137b859f4b2b7796ccc8
Aaron Didner <didnera@flip1.engr.oregonstate.edu>
2019-04-02 23:43:08 -0700

    minor fix

--- Assignment-1/insertionsort.py
+++ Assignment-1/insertionsort.py
@@ -3,20 +3,20 @@ import re
 
 def sort(list):
 	for x in range(0,len(list)):
-		print(list)
+		#print(list)
 		for i in range(x,-1,-1):
 			#if I am the first element
 			if x == 0:
-				print("first")
+				#print("first")
 				continue
 			#if I am greater than the left element
 			if list[x] >= list[x-1]:
-				print("greater than left: "+ str(list[x]) + ", "+ str(list[x-1]))
+				#print("greater than left: "+ str(list[x]) + ", "+ str(list[x-1]))
 				continue
 			#if There is no left element
 			if i == 0:
-				print("i of 0")
-				print(list)
+				#print("i of 0")
+				#print(list)
 				temp=list[x]
 				del list[x]
 				list.insert(0,temp)
@@ -26,8 +26,8 @@ def sort(list):
 			#if I am between 2 elements
 			if i-1 >= 0:
 				if list[i-1] < list[x] and list[x] <= list[i]:
-						print("in 2 between")
-						print(list)
+						#print("in 2 between")
+						#print(list)
 						temp = list[x]
 						del list[x]
 						list.insert(i,temp)
@@ -48,25 +48,35 @@ def read_from_file(content, fname):
 	return content
 	
 	
-def sort_everything(content):
+def sort_everything(content, fwrite):
+	f = open(fwrite, "w")
+	
+	
 	for x in range(0,len(content)):	
-		print(content[x])
+	
+		#print(content[x])
 		current = content[x].split()
 		current = list(map(int, current))
 		sorted_list = sort(current)
 		print(sorted_list)
-		print("-----------")
-	
+		
+		
+		for item in sorted_list:
+			f.write("%s " % str(item))
+		f.write("\n")
+		
+		print("\n")
+	f.close()
 	
 	
 def main():
 	fname = "data.txt"
 	content = []
-	
+	fwrite = "insert.txt"
 		
 	content = read_from_file(content, fname)
 	
-	sort_everything(content)
+	sort_everything(content, fwrite)
 	
 
 	
