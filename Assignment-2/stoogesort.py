86a6886acfb79ad9dbffd2b6b7cbf8300163b09d
Aaron Didner <didnera@flip3.engr.oregonstate.edu>
2019-04-10 18:07:46 -0700

    finished stooge from file

--- Assignment-2/stoogesort.py
+++ Assignment-2/stoogesort.py
@@ -16,12 +16,44 @@ def stooge_sort(list_ints):
 	return list_ints
 
 
+def read_from_file(fname):
+	content = []
+	with open(fname) as f:
+		content = f.readlines()
+		
+	for x in range(0,len(content)):
+		content[x] = re.sub('\r','',content[x])
+		content[x] = re.sub('\n','',content[x])
+		content[x] = content[x].split()
+		content[x] = list(map(int, content[x]))
+	#print(content)
+	return content
 		
 def main():
-	input_array = [5,7,1,9,8,11,3,1]
-	n = len(input_array)
-	stooge_sort(input_array)
-	print(input_array)
+	
+	fname = "data.txt"
+	fwrite = "stooge.txt"
+	
+	
+	
+	
+	
+	lists_to_sort = read_from_file(fname)
+	
+	
+	f = open(fwrite, "w")
+	for x in range(0, len(lists_to_sort)):
+		
+		sorted_list = stooge_sort(lists_to_sort[x])
+		
+		for item in sorted_list:
+			f.write("%s " % str(item))
+		f.write("\r\n")
+	
+	
+	
+	
+	
 
 
 main()
