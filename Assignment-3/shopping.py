81a3d578830b5893f8db2b951f24749b1bc9e63c
Aaron Didner <didnera@flip1.engr.oregonstate.edu>
2019-04-16 19:46:57 -0700

    finished index tracking and pretty printing

--- Assignment-3/shopping.py
+++ Assignment-3/shopping.py
@@ -73,40 +73,86 @@ def print_filtered_data(global_test_cases):
 		
 		
 def run_napsack(global_test_cases):
+	
+	list_total_prices = []
 	for list_item, list_fam in global_test_cases:
+		current_total = 0
 		for current_fam in list_fam:
-			napsack(current_fam, current_fam.weight_capacity, list_item)
+			current_fam.list_of_item_indexs, result = napsack(current_fam.weight_capacity, list_item, len(list_item))
+			current_total += result
+			print(current_fam.list_of_item_indexs)
+		list_total_prices.append(current_total)	
+	return global_test_cases,list_total_prices
+			
+def napsack(W, list_items, n):
+		
+	list_indexs = []	
+		
+	K = [[0 for w in range(W + 1)]for i in range(n + 1)]
+	
+	for i in range(n + 1): 
+		for w in range(W + 1): 
+			if i == 0 or w == 0: 
+				K[i][w] = 0
+			elif list_items[i - 1].weight <= w: 
+				K[i][w] = max(list_items[i - 1].price  
+				+ K[i - 1][w - list_items[i - 1].weight], 
+				K[i - 1][w]) 
+			else: 
+				K[i][w] = K[i - 1][w]
+	
+	res = K[n][W] 
+	returner_res = K[n][W]
+      
+	w = W 
+	for i in range(n, 0, -1): 
+		if res <= 0: 
+			break
+			
+		if res == K[i - 1][w]: 
+			continue
+		else:
+			#print("item Index: "+str(list_items[i - 1].index + 1)) 
+			list_indexs.insert(0,list_items[i - 1].index + 1)
+			res = res - list_items[i - 1].price 
+			w = w - list_items[i - 1].weight
+			
+	return list_indexs, returner_res
+			
 
-def napsack(current_fam, weight_capacity, list_items, current_size):
-	if len(list_items) == 0 or weight_capacity == 0:
-		return 
+def prettyprint(global_test_cases,list_total_prices, fwrite):
 	
+	f = open(fwrite, "w")
 	
-	#run recursive part of napsack and put the values back into the index
-	#tracking part of the class
-	
-	
-	#if list_items[n-1].weight > weight_capacity:
-	#	return napsack(current_fam, weight_capacity,list_items, n-1)
-	#else:
-	#	return max(list_items[n-1] + napsack(current_fam, weight_capacity - list_items[n-1], weight_capacity, list_items, n-1), napsack(current_fam, weight_capacity,list_items, n-1))
-	
-	
-#def prettyprint():	
-
-
+	counter = 1
+	for list_item, list_fam in global_test_cases:
+		f.write("Test Case %s \n" % str(counter))
+		f.write("Total Price %s \n" % str(list_total_prices[counter-1]))
+		f.write("Member Items: \n")
+		i = 1
+		for current_fam in list_fam:
+			f.write("%s: "% str(i))
+			for x in current_fam.list_of_item_indexs:
+				f.write("%s "% str(x))
+			f.write("\n")
+			i+=1
+		f.write("\n")
+		
+		counter+=1
 def main():
 	fname = "shopping.txt"
+	fwrite = "results2.txt"
 	global_test_cases = []
 	
 	#run the section that gets all the data from the files
 	content = ReadTestData(fname)
 	global_test_cases = FilterIntoClasses(content, global_test_cases)
 	
-	print_filtered_data(global_test_cases)
+	#print_filtered_data(global_test_cases)
 	
-	run_napsack(global_test_cases)
+	global_test_cases,list_total_prices = run_napsack(global_test_cases)
 	
+	prettyprint(global_test_cases,list_total_prices, fwrite)
 
 
 
