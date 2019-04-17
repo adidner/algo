10ae8d653f56f94e2dd26bd434fe47c396015c78
Aaron Didner <didnera@flip1.engr.oregonstate.edu>
2019-04-16 20:34:06 -0700

    commenting done

--- Assignment-3/shopping.py
+++ Assignment-3/shopping.py
@@ -1,13 +1,14 @@
 import re
 
 
-
+#item class to deal with the item characteristics of price, weight and index
 class item:
 	def __init__(self, price, weight, index):
 		self.price = price
 		self.weight = weight
 		self.index = index
 
+#class family member to deal with the weight capacity and list of item indexs assocaited with each family member
 class family_member:
 	def __init__(self, weight_capacity):
 		self.weight_capacity = weight_capacity
@@ -16,7 +17,9 @@ class family_member:
 	def printer(self):
 		for x in range(0, len(self.list_of_item_indexs)):
 			print(str(self.list_of_item_indexs[x])+" ", end =" ")
-		
+			
+
+#the function used to read in test data from a file		
 def ReadTestData(fname):
 	content = []
 	with open(fname) as f:
@@ -26,8 +29,9 @@ def ReadTestData(fname):
 		content[x] = re.sub('\r','',content[x])
 		content[x] = re.sub('\n','',content[x])
 	
-	#print(content)
 	return content
+
+
 	
 def FilterIntoClasses(content, global_test_cases):
 	current_line = 1
@@ -57,6 +61,7 @@ def FilterIntoClasses(content, global_test_cases):
 		
 	return global_test_cases
 
+#a function used to make sure the data was correctly read from a file and put into the corresponding classes
 def print_filtered_data(global_test_cases):
 	for list_item, list_fam in global_test_cases:
 		print("LIST ITEMS")
@@ -71,7 +76,7 @@ def print_filtered_data(global_test_cases):
 
 		
 		
-		
+# a function meant to run the napsack algorithm on all the test cases and corresponding sub peices		
 def run_napsack(global_test_cases):
 	
 	list_total_prices = []
@@ -80,46 +85,53 @@ def run_napsack(global_test_cases):
 		for current_fam in list_fam:
 			current_fam.list_of_item_indexs, result = napsack(current_fam.weight_capacity, list_item, len(list_item))
 			current_total += result
-			print(current_fam.list_of_item_indexs)
+			#print(current_fam.list_of_item_indexs)
 		list_total_prices.append(current_total)	
 	return global_test_cases,list_total_prices
-			
-def napsack(W, list_items, n):
+
+# the actual running of the napsack algorithm
+def napsack(max_weight, list_items, size):
 		
 	list_indexs = []	
 		
-	K = [[0 for w in range(W + 1)]for i in range(n + 1)]
+	table = [[0 for max_weight in range(max_weight + 1)]for i in range(size + 1)]
 	
-	for i in range(n + 1): 
-		for w in range(W + 1): 
-			if i == 0 or w == 0: 
-				K[i][w] = 0
-			elif list_items[i - 1].weight <= w: 
-				K[i][w] = max(list_items[i - 1].price  
-				+ K[i - 1][w - list_items[i - 1].weight], 
-				K[i - 1][w]) 
+	for i in range(size+1): 
+		
+		for w in range(max_weight+1): 
+			if i==0 or w==0: 
+				
+				table[i][w] = 0
+				
+			elif list_items[i-1].weight <= w: 
+				
+				table[i][w] = max(
+				list_items[i-1].price + table[i-1][w - list_items[i-1].weight], table[i-1][w]
+				)
+				
 			else: 
-				K[i][w] = K[i - 1][w]
+				
+				table[i][w] = table[i-1][w]
 	
-	res = K[n][W] 
-	returner_res = K[n][W]
+	result = table[size][max_weight] 
+	returner_res = table[size][max_weight]
       
-	w = W 
-	for i in range(n, 0, -1): 
-		if res <= 0: 
+	#the back trace section to regain indexes
+	w = max_weight 
+	for i in range(size, 0, -1): 
+		if result <= 0: 
 			break
 			
-		if res == K[i - 1][w]: 
+		if result == table[i - 1][w]: 
 			continue
-		else:
-			#print("item Index: "+str(list_items[i - 1].index + 1)) 
+		else: 
 			list_indexs.insert(0,list_items[i - 1].index + 1)
-			res = res - list_items[i - 1].price 
+			result = result - list_items[i - 1].price 
 			w = w - list_items[i - 1].weight
 			
 	return list_indexs, returner_res
 			
-
+#print to the file with the format specified in the homework assignment
 def prettyprint(global_test_cases,list_total_prices, fwrite):
 	
 	f = open(fwrite, "w")
@@ -139,19 +151,27 @@ def prettyprint(global_test_cases,list_total_prices, fwrite):
 		f.write("\n")
 		
 		counter+=1
+		
+		
 def main():
+	#declar global variables like file names and the top level array of test cases
 	fname = "shopping.txt"
-	fwrite = "results2.txt"
+	fwrite = "results.txt"
 	global_test_cases = []
 	
 	#run the section that gets all the data from the files
 	content = ReadTestData(fname)
+	
+	#put all the data from the file into the list of classes
 	global_test_cases = FilterIntoClasses(content, global_test_cases)
 	
 	#print_filtered_data(global_test_cases)
 	
+	#run the napsack algorithm and get the return data back in the lists associated witht eh global test array (list_items, list_family)
+	#also got back the sum of total value collected by the family
 	global_test_cases,list_total_prices = run_napsack(global_test_cases)
 	
+	#print all the data to a file
 	prettyprint(global_test_cases,list_total_prices, fwrite)
 
 
