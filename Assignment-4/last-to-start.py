62980bd00373a924bb70449fad5e06644e8fce84
Aaron Didner <didnera@flip2.engr.oregonstate.edu>
2019-04-23 23:33:21 -0700

    added printing and last2start sections

--- Assignment-4/last-to-start.py
+++ Assignment-4/last-to-start.py
@@ -1,4 +1,7 @@
 import re
+import operator
+
+
 
 #item class to deal with the item characteristics of price, weight and index
 class event:
@@ -28,15 +31,12 @@ def FilterIntoClasses(content):
 	global_test_cases = []
 	current_line = 0
 	
-	
-	
-	
 	while current_line < len(content):
 		list_events = []
 		
 		#items
 		range_end = int(content[current_line])
-		print(range_end)
+		#print(range_end)
 		for i in range(0, range_end):
 			current_line+=1
 			minilist = content[current_line].split()
@@ -64,6 +64,70 @@ def print_filtered_data(global_test_cases):
 		print("-------------------------------------")
 
 
+def last2start(gloabl_test_cases):
+	#going through everything in the global list
+	i = 1
+	print(" ")
+	for current_test_case in gloabl_test_cases:
+		print("Set "+ str(i))
+		#sort by start time greatest to smallest
+		sorted_list = sorted(current_test_case, key=operator.attrgetter('start'))
+		sorted_list.reverse()
+		finallist = []
+		
+		recursiveelement(sorted_list, finallist)
+		
+		print("Number of activities selected = "+str(len(finallist)))
+		print("Activities:", end=" ")
+		finallist.reverse()
+		for current in finallist:
+			print(str(current.index),end=" ")
+		#print_list(finallist)
+		print(" ")
+		print(" ")
+		i+=1
+		
+
+def recursiveelement(sorted_list, finallist):
+	
+	
+	
+	
+	#find the element with the latest (greatest) start time
+	greatest = sorted_list[0]
+	
+	i = 1
+	length = len(sorted_list)
+	while i < length:
+		#eliminate all that conflict with item
+		if sorted_list[i].end > greatest.start:
+			#print_list(sorted_list)
+			del sorted_list[i]
+			#print_list(sorted_list)
+			length-= 1
+		else:
+			i+=1
+	
+			
+	#recursively add it to an array and run it again on the resulting array
+	del sorted_list[0]
+	
+	
+	
+	finallist.append(greatest)
+	
+	#base case
+	if len(sorted_list) == 0:
+		return
+	
+	recursiveelement(sorted_list, finallist)
+
+
+def print_list(sorted_list):
+	for current in sorted_list:
+		print(str(current.index)+": "+ str(current.start) + ", " + str(current.end))
+	print("----------------")
+
 def main():
 	fname = "act.txt"
 	global_test_cases = []
@@ -71,7 +135,7 @@ def main():
 	content = ReadTestData(fname)
 	#print(content)
 	gloabl_test_cases = FilterIntoClasses(content)
-	print_filtered_data(gloabl_test_cases)
-	
+	#print_filtered_data(gloabl_test_cases)
+	last2start(gloabl_test_cases)
 		
 main()
