6ba01897905730495ed40e3b32de45e64e9c9a8d
Aaron Didner <didnera@flip3.engr.oregonstate.edu>
2019-05-07 20:38:19 -0700

    fixed filter into classes error, fixing modified bfs

--- Assignment-5/babyfacevsheels.py
+++ Assignment-5/babyfacevsheels.py
@@ -10,6 +10,7 @@ class node:
 		self.name = name
 		self.type = ""
 		self.neighbors = []
+		self.color = 'white'
 	
 	def add_neighbor(self, node):
 		if node not in self.neighbors:
@@ -29,16 +30,16 @@ class Graph:
 			
 	def add_edge(self, start, end):
 		if start in self.nodes and end in self.nodes:
+			#print("in if")
+			#print("start: "+ start)
+			#print("end: "+ end)
 			self.nodes[start].add_neighbor(end)
 			self.nodes[end].add_neighbor(start)
 			return True
 		else:
 			return False
 	
-	def get_node(self, name):
-		for key in sorted(list(self.nodes.keys())):
-			if key == name:
-				return key
+	
 	
 	def print_graph(self):
 		for key in sorted(list(self.nodes.keys())):
@@ -72,20 +73,61 @@ def ReadTestData(fname):
 def FilterIntoClasses(content):
 	main_graph = Graph()
 	size=int(content[0])
-	for i in range(1, size):
+	for i in range(1, size+1):
 		new_node = node(str(content[i]))
 		main_graph.add_node(new_node)
+	#print(main_graph.nodes)
 	
-	
-	#print(size)
-	#print(int(content[size+1]))
 	for i in range(1, int(content[size+1])+1):
 		minilist = content[i+size+1].split()
+		#print("minilist: ",end="")
 		#print(minilist)
-		#main_graph.add_edge(main_graph.get_node(minilist[0]), main_graph.get_node(minilist[1]))
 		main_graph.add_edge(minilist[0], minilist[1])
 	main_graph.print_graph()
 	return main_graph
+	
+	
+def modified_bfs(main_graph, root):
+	currenttype = good
+	queue = []
+	queue.append(root)
+	
+	while len(queue) != 0:
+		current = queue.pop()
+		for i in current.neighbors:
+			current_node=main_graph.nodes[i]
+			if current_node.type=='':
+				current_node.type = currenttype
+				queue.append(current_node)
+			elif current_node.type==currenttype:
+				impossible()
+				return
+		flipcurrenttype(currenttype)
+		
+	main_graph.print_result()
+	
+	
+	
+	#Looping through ever node
+		#if you find one without a color run this again on that node
+
+def impossible():
+	print("Impossible")
+	
+
+def oppositeofcurrenttype(currenttype):
+	if currenttype==good:
+		return bad
+	elif currenttype==bad:
+		return good
+	
+def flipcurrenttype(currenttype):
+	if currenttype==good:
+		currenttype==bad
+	elif currenttype==bad:
+		currenttype==good
+	
+
 
 def main():
 	
@@ -95,9 +137,12 @@ def main():
 	
 	content = ReadTestData(fname)
 	
-	FilterIntoClasses(content)
+	main_graph = FilterIntoClasses(content)
 
-
+	root=main_graph.nodes[str(content[1])]
+	
+	
+	modified_bfs(main_graph,root)
 
 
 
